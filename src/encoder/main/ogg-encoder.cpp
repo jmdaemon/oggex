b79aed3abd3d8001043726d46c28976d5d064a6d
Joseph Diza <josephm.diza@gmail.com>
2020-12-07 17:35:18 -0800

    Add ogg-encoder.cpp stub and bazel BUILD file

---
+++ src/encoder/main/ogg-encoder.cpp
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+
+int writeToFile(std::string filepath ) {
+  return 0;
+}
+
+int main () {
+  std::cout << "Testing bazel build file...\n"; 
+}
