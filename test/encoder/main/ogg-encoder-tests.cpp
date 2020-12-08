1b316b0d9da6851b10b58ac532df93dbe70399df
Joseph Diza <josephm.diza@gmail.com>
2020-12-07 17:31:57 -0800

    Add catch.hpp header to unit tests

---
+++ test/encoder/main/ogg-encoder-tests.cpp
@@ -0,0 +1,12 @@
+#include "catch.hpp"
+
+#include <string>
+
+bool embedInImage(std::string imgPath, std::string oggPath) {
+  return embedInImage(imgPath, oggPath);
+}
+
+
+TEST_CASE( "OGG file is embedded in image" ,  "[encoder]") {
+  // Fill in with files of various formats.
+}
