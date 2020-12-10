9f8de27638db55c916f9caaa24f051971112a19c
Joseph Diza <josephm.diza@gmail.com>
2020-12-09 16:21:06 -0800

    Remove SRC_LIBS, TEST_LIBS vars

--- src/encoder/main/ogg-encoder.cpp
+++ src/encoder/main/ogg-encoder.cpp
@@ -1,11 +1,42 @@
+#include "oggex.hpp"
+
+#include <ogg/ogg.h>
+
 #include <stdlib.h>
+#include <filesystem>
+
 #include <iostream>
 #include <fstream>
 
-int writeToFile(std::string filepath ) {
-  return 0;
-}
+//namespace fs = std::filesystem;
 
-int main () {
-  std::cout << "Testing bazel build file...\n"; 
+//class File {
+  //std::string filepath;
+
+  //File(const std::string &filepath) {
+    //this->filepath = filepath;
+  //}
+
+  
+//}
+
+
+//bool openFile(const fs::path& filepath, fs::file_status status = fs::file_status{}) {
+  //if ( ! std::filesystem::exists(&filepath)) {
+      //throw (filepath + " not found");
+      //} 
+  //std::fstream file;
+  //file.open(filepath, ios::
+//}
+
+int main (int argc, char** argv) {
+  //std::cout << "Testing bazel build file...\n"; 
+
+  if (argc < 2) {
+    // report version
+    std::cout << argv[0] << " Version " << oggex_VERSION_MAJOR << "."
+              << oggex_VERSION_MINOR << std::endl;
+    std::cout << "Usage: " << argv[0] << " number" << std::endl;
+    return 1;
+  }
 }
