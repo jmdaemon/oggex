3ebf90b022c0903a531f08442b09337727fde2d8
Joseph Diza <josephm.diza@gmail.com>
2020-12-10 21:44:53 -0800

    Update cmake build script

--- build_cmake.sh
+++ build_cmake.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-cd build
-cmake -G Ninja ../
+
+function cmakeBuildOld() {
+    cd build
+    cmake -G Ninja ../
+}
+
+function cmakeBuild() {
+    CC=clang CXX=clang++ cmake -S . -B build -G Ninja
+}
+
+cmakever="$( cmake --version | grep version | cut -d ' ' -f 3 )"
+required="3.0.0"
+ if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then 
+     cmakeBuild
+ else
+     cmakeBuildOld
+ fi
