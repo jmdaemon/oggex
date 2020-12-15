33c4cb50dd2606d55925233ee6e0595fbe562e3c
Joseph Diza <josephm.diza@gmail.com>
2020-12-15 11:12:15 -0800

    Remove comment in ogg-encoder-tests

--- test/encoder/ogg-encoder-tests.cpp
+++ test/encoder/ogg-encoder-tests.cpp
@@ -8,11 +8,11 @@ bool embedInImage(std::string imgPath, std::string oggPath) {
   return embedInImage(imgPath, oggPath);
 }
 
-int main(int argc, char** arv) {
+int main(int argc, char** argv) {
   return 0;
 }
 
 TEST_CASE( "OGG file is embedded in image") {
   CHECK(embedInImage("inputFile1.png", "outputFile.png") == true);
-  // Fill in with files of various formats.
+  //CHECK(getTags("inputFile1") != 0);
 }
