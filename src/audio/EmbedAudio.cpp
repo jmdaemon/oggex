524e6d13f0ca07c944311e78de1dd90efaf297b6
Joseph Diza <josephm.diza@gmail.com>
2020-12-15 15:53:08 -0800

    Fix compiler errors in EmbedAudio.cpp

--- src/audio/EmbedAudio.cpp
+++ src/audio/EmbedAudio.cpp
@@ -1,32 +1,42 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include <algorithm>
 #include <cctype>
 #include <tuple>
 #include <filesystem>
+#include <map>
+#include <exception>
+
+#include <cstdlib>
+#include <cstdio>
 #include <cstdint>
 
 #include <unistd.h>
+#include <getopt.h>
+
+#include <EmbedAudio.h>
 
 using namespace std;
 namespace fs = std::filesystem;
 
 string toLowerCase(const fs::path& filePath) {
-  string filePath = filePath.string();
-  transform(filePath.begin(), filePath.end(), data.begin(),
+  string fpath = filePath.string();
+  transform(fpath.begin(), fpath.end(), fpath.begin(), 
       [](unsigned char c ) { return tolower(c); }); 
+  return fpath;
 }
 
 static void showUsage(std::string programName) { 
-  cerr << "Usage:" << argv[0] << " [audio_file] [image_file]\n"; 
-       << "Options:\n";
-       << "\t-h, --help\t\tShow this help message\n";
-       << "\t-f, --fast\t\tLower image quality\n";
+  cerr << "Usage:" << programName << " [audio_file] [image_file]\n" 
+       << "Options:\n"
+       << "\t-h, --help\t\tShow this help message\n"
+       << "\t-f, --fast\t\tLower image quality\n"
        << endl;
 }
 
 static bool bestQuality = true;
-const static struct option runtimeOptions [] = {
+static struct option runtimeOptions [] = {
   {"help",  no_argument,    0, 'h'},
   {"fast",  no_argument,    0, 'f'},
   {0,0,0,0}, 
@@ -44,7 +54,8 @@ tuple<fs::path, fs::path> parseOptions(int argc, char** argv) {
   c = getopt_long (argc, argv, "hf", runtimeOptions, &option_index);
 
   if (c == -1)
-    break;
+    throw std::exception();
+    //break;
 
   switch(c) {
     case 'h':
@@ -61,12 +72,13 @@ tuple<fs::path, fs::path> parseOptions(int argc, char** argv) {
   }
 
   if (optind < 2) {
-    auto imageFilePath = argv[optind];
-    auto audioFilePath = argv[optind + 1];
+    fs::path imageFilePath = argv[optind];
+    fs::path audioFilePath = argv[optind + 1];
     if (!imageFilePath.empty() && audioFilePath.empty()) {
       return tuple<fs::path, fs::path>{imageFilePath, audioFilePath};
     }
   }
+  return std::make_tuple("", "");
 }
 
 const static map<int, string> ValidImageFileExtensions = {
@@ -81,7 +93,8 @@ int checkFile(fs::path filePath) {
   int fileExists = 0;
   string extension = toLowerCase(filePath.extension());
   for (int i = 0; i < 5; i++) {
-    if(ValidImageFileExtensions[i] == extension) {
+    //if(ValidImageFileExtensions[i] == extension) {
+    if(ValidImageFileExtensions.at(i) == extension) {
       fileExists++;
       break;
     }
@@ -107,16 +120,40 @@ bool imageNotCorrupted(fs::path imageFilePath) {
   return true;
 }
 
+void cleanTempFiles() {
+  //Marker
+  //// Clean up tempFiles
+  //// Temp file removal
+  ////string tempLogFile = "nul";
+  ////string tempAudioFile = "out.ogg";
+  //fs::path tempLogFile = "nul";
+  //fs::path tempAudioFile = "out.ogg";
+  ////f.open(tempAudioFile.c_str(), ifstream::in | ifstream::binary);
+  //audioFile.open(tempAudioFile, ifstream::in | ifstream::binary);
+  //if (audioFile.is_open()) {
+    //audioFile.close();
+    //remove(audioFile);
+  //}
+}
+
 
 int getFile(int argc, char** argv) {
   tuple<fs::path, fs::path> mediaFiles;
   try {
-    mediaFiles = tuple<fs::path, fs::path> mediaFiles = parseOptions(argc, argv);
+    mediaFiles = parseOptions(argc, argv);
   } catch (const std::exception&) {
     return -1;
   }
   fs::path audioFilePath = get<0>(mediaFiles);
-  fs::path imageFilePath = get(1)(mediaFiles);
+  fs::path imageFilePath = get<1>(mediaFiles);
+  //fs::path audioFilePath = mediaFiles.get(0);
+  //fs::path imageFilePath = mediaFiles.get(0);
+  //fs::path audioFilePath = get<0>(mediaFiles);
+  //fs::path imageFilePath = get(1)(mediaFiles);
+
+  //fs::path audioFilePath, imageFilePath;
+  //auto [audioFilePath, imageFilePath] = get<0>(mediaFiles);
+
 
   vector<int> sounds; 
   int imageFileExists = checkFile(imageFilePath);
@@ -137,43 +174,32 @@ int getFile(int argc, char** argv) {
     return -1;
   } 
 
-  // Clean up tempFiles
-  // Temp file removal
-  //string tempLogFile = "nul";
-  //string tempAudioFile = "out.ogg";
-  fs::path tempLogFile = "nul";
-  fs::path tempAudioFile = "out.ogg";
-  //f.open(tempAudioFile.c_str(), ifstream::in | ifstream::binary);
-  audioFile.open(tempAudioFile, ifstream::in | ifstream::binary);
-  if (audioFile.is_open()) {
-    audioFile.close();
-    remove(audioFile);
-  }
+  
 
-  unsigned int maxTagLength = 100;
-  vector<size_t> soundSizes;
-  vector<string> soundTags;
-  vector<bool> canQualityGain;
-  for (unsigned int i = 0; i < sounds.size(); ++i) {
+  //unsigned int maxTagLength = 100;
+  //vector<size_t> soundSizes;
+  //vector<string> soundTags;
+  //vector<bool> canQualityGain;
+  //for (unsigned int i = 0; i < sounds.size(); ++i) {
 
-    soundSizes.push_back(0);
-    canQualityGain.push_back(true);
+    //soundSizes.push_back(0);
+    //canQualityGain.push_back(true);
 
-    soundTags.push_back(argv[sounds[i]]);
-    ext = getExtension(soundTags[i]);
+    //soundTags.push_back(argv[sounds[i]]);
+    //ext = getExtension(soundTags[i]);
 
-    soundTags[i] = soundTags[i].substr(0, soundTags[i].length() - ext.length());
-    int j = soundTags[i].length() - 1;
+    //soundTags[i] = soundTags[i].substr(0, soundTags[i].length() - ext.length());
+    //int j = soundTags[i].length() - 1;
 
-    for (; j >= 0; --j) {
-      if (soundTags[i][j] == '\\' || soundTags[i][j] == '/') break;
-    }
+    //for (; j >= 0; --j) {
+      //if (soundTags[i][j] == '\\' || soundTags[i][j] == '/') break;
+    //}
 
-    if (j >= 0) soundTags[i] = soundTags[i].substr(j + 1, soundTags[i].length() - (j + 1));
-    if (soundTags[i].length() > maxTagLength - 2) soundTags[i] = soundTags[i].substr(0, maxTagLength - 2);
-    soundTags[i].insert(0, "[");
-    soundTags[i] += "]";
-  }
+    //if (j >= 0) soundTags[i] = soundTags[i].substr(j + 1, soundTags[i].length() - (j + 1));
+    //if (soundTags[i].length() > maxTagLength - 2) soundTags[i] = soundTags[i].substr(0, maxTagLength - 2);
+    //soundTags[i].insert(0, "[");
+    //soundTags[i] += "]";
+  //}
   return 0;
 }
 
