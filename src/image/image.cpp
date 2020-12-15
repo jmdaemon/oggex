f56e3992630abd2822d6d55a1cc6cb5175662fb0
Joseph Diza <josephm.diza@gmail.com>
2020-12-14 16:48:11 -0800

    Remove unneeded dependencies

--- src/image/image.cpp
+++ src/image/image.cpp
@@ -1,36 +1,19 @@
-#include <opencv2/opencv.hpp>
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-
 #include <iostream>
 #include <string>
 #include <stdlib.h>
+#include <vector>
 
 #include "image.hpp"
 
-//using namespace cv;
-//Image 
-//{ 
-  //private:
-    //std::string imagePath;
-  //public:
+using namespace std;
+
 Image::Image(std::string imagePath) { 
   setImg(imagePath); 
 } 
 
-int Image::readImage() { 
-  cv::Mat img = cv::imread(imagePath, cv::IMREAD_COLOR); 
-  if (img.empty()) { 
-    std::cout << "Could not read image : " << imagePath << std::endl; 
-    return 1; 
-  } 
+int Image::readImage() {
+}
 
-  cv::imshow("Display window", img);
-  int k = cv::waitKey(0); 
-  if(k == 's') 
-  {
-    cv::imwrite(imagePath, img);
-  }
-  return 0;
-  }
+
+
+
