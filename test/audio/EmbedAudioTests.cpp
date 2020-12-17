73addd3afc31837718d38a06d718ce053f0cb1de
Joseph Diza <josephm.diza@gmail.com>
2020-12-16 20:14:19 -0800

    Remove dead code for showUsage test

--- test/audio/EmbedAudioTests.cpp
+++ test/audio/EmbedAudioTests.cpp
@@ -8,38 +8,6 @@
 
 using namespace std;
 
-//std::string testShowUsage(std::string programName) {
-  //stringstream buffer;
-  //streambuf *sbuf = std::cerr.rdbuf();
-  //cerr.rdbuf(standardOutput.rdbuf());
-  //showUsage(programName);
-  //string result = standardOutput.str();
-  //cout << result;
-  //return result;
-//}
-
-//string showUsageOutput(std::string programName) {
-  //stringstream ss;
-   //ss  << "Usage:" << programName << " [audio_file] [image_file]\n"
-       //<< "Options:\n"
-       //<< "\t-h, --help\t\tShow this help message\n"
-       //<< "\t-f, --fast\t\tLower image quality\n"
-       ////<< "Fuck my shit up"
-       //<< std::endl;
-   //std::cout << ss.str();
-   //return ss.str();
-//}
-
-//void logToFile(std::string fileName) { 
-  //ofstream log_file(fileName, std::ios::binary);
-
-//}
-
-//void redirectOutput() {
-  //streambuf* originalCerrStreamBuf = cerr.rdbuf();
-  //ostringstream strCerr;
-  //cout.rdbuf( strCerr.rdbuf() ); 
-//}
 
 TEST_CASE("Audio files can be embedded into image files") {
 
@@ -53,7 +21,4 @@ TEST_CASE("Audio files can be embedded into image files") {
     CHECK(toLowerCase(".PNG") == ".png");
   }
 
-  //SUBCASE("Test showUsage()") {
-    //REQUIRE(testShowUsage("oggex") == showUsageOutput("oggex"));
-  //}
 }
