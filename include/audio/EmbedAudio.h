44853918342868ad01d4da9c7aae252e2f8acd3a
Joseph Diza <josephm.diza@gmail.com>
2020-12-16 21:31:53 -0800

    Write parseOptions test to check exception

--- include/audio/EmbedAudio.h
+++ include/audio/EmbedAudio.h
@@ -3,8 +3,10 @@
 
 #include <string>
 #include <filesystem>
+#include <tuple>
 
 std::string toLowerCase(const std::filesystem::path& filePath);
 void showUsage(std::string programName);
+std::tuple<std::filesystem::path, std::filesystem::path> parseOptions(int argc, char** argv);
 
 #endif
