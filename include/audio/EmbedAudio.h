faa420a24303c19d8ad02c0a6a9fb7ff72da18df
Joseph Diza <josephm.diza@gmail.com>
2020-12-17 15:33:44 -0800

    Remove dead function prototypes in EmbedAudio header

--- include/audio/EmbedAudio.h
+++ include/audio/EmbedAudio.h
@@ -7,9 +7,6 @@
 
 std::string toLowerCase(const std::filesystem::path& filePath);
 void showUsage(std::string programName);
-/*std::tuple<std::filesystem::path, std::filesystem::path> parseOptions(int argc, char** argv);*/
-/*std::map<std::filesystem::path, std::filesystem::path> parseOptions(int argc, char** argv);*/
-/*std::map<int, std::filesystem::path> parseOptions(int argc, char** argv);*/
 std::map<int, std::string> parseOptions(int argc, char** argv);
 
 #endif
