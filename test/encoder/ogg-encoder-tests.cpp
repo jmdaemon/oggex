a707336f581fc25e37c4d97f9686a8f02ea86fd2
Joseph Diza <josephm.diza@gmail.com>
2020-12-16 20:23:24 -0800

    Remove main() in encoder

--- test/encoder/ogg-encoder-tests.cpp
+++ test/encoder/ogg-encoder-tests.cpp
@@ -8,10 +8,6 @@ bool embedInImage(std::string imgPath, std::string oggPath) {
   return embedInImage(imgPath, oggPath);
 }
 
-int main(int argc, char** argv) {
-  return 0;
-}
-
 TEST_CASE( "OGG file is embedded in image") {
   CHECK(embedInImage("inputFile1.png", "outputFile.png") == true);
   //CHECK(getTags("inputFile1") != 0);
