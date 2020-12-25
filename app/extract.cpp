ef5e46fb3ebeb4fb7c2c9312a895607a5d169b65
Joseph Diza <josephm.diza@gmail.com>
2020-12-24 23:52:10 -0800

    Implement audio extraction from image

--- app/extract.cpp
+++ app/extract.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
+#include <string>
 #include <filesystem>
 
 #include "ExtractAudio.h"
 
+using namespace std;
 namespace fs = std::filesystem;
 
 int main(int argc, char** argv) {
-  fs::path filepath = argv[0];
+  //string path;
+  //if (argc > 0 && argc < 2) 
+    //std::string path = argv[1];
+
+  //fs::path filepath = argv[0];
+  //fs::path filepath = path;
+  fs::path filepath = argv[1];
   extract(filepath);
   return 0;
 }
--- src/audio/ExtractAudio.cpp
+++ src/audio/ExtractAudio.cpp
@@ -7,6 +7,7 @@
 #include <streambuf>
 #include <exception>
 #include <locale>
+#include <regex>
 
 #include <fmt/core.h>
 #include <fmt/printf.h>
@@ -21,26 +22,43 @@ namespace fs = std::filesystem;
 size_t getAudioOffset(ifstream& file, const char* search_term = "OggS") {
   file.seekg(0, ios::end);
   size_t file_size = file.tellg();
-  fmt::print("\nSize of embedded file: \t{} bytes\n", file_size);
+  fmt::print("\nSize of embedded file: \t\t{} bytes\n", file_size);
 
   file.seekg(0, ios::beg);
-  string file_content;
-  file_content.reserve(file_size);
-  char buffer[16384];
-  streamsize chars_read;
 
-  while (file.read(buffer, sizeof buffer), chars_read = file.gcount())
-      file_content.append(buffer, chars_read);
+  ostringstream fileContents;
+  fileContents << file.rdbuf();
+  string filedata = fileContents.str();
+  //string filedata = file_content.str();
 
-  size_t search_term_size = strlen(search_term);
-
-  size_t offset = 0; 
+  //std::string key = "\x39\xb5\x7d\xc6";
+  //string searchTerm = "\0x4\0x67\0x67\0x53\0\2";
+  //string searchTerm = "\x4\x67\x67\x53";
   size_t found_at = 0;
-  if (file.eof()) { 
-    for (; file_size > offset && (found_at = file_content.find(search_term, offset)) != string::npos; 
-        offset = found_at + search_term_size)
-      break; 
-  }
+  found_at = filedata.find(search_term);
+  //found_at = filedata.find(searchTerm);
+  //auto result = search(begin(filedata), end(filedata), begin(searchTerm), end(searchTerm));
+  //if (end(filedata) != result) {
+    //found_at = result - filedata.begin();
+  //}
+
+  //string file_content;
+  //file_content.reserve(file_size);
+  //char buffer[16384];
+  //streamsize chars_read;
+
+  //while (file.read(buffer, sizeof buffer), chars_read = file.gcount())
+      //file_content.append(buffer, chars_read);
+
+  //size_t search_term_size = strlen(search_term);
+
+  //size_t offset = 0; 
+  //size_t found_at = 0;
+  //if (file.eof()) { 
+    //for (; file_size > offset && (found_at = file_content.find(search_term, offset)) != string::npos; 
+        //offset = found_at + search_term_size)
+      //break; 
+  //}
 
   if (file_size == found_at) {
     fmt::fprintf(cerr, "file_size: {} == found_at: {}. Search term not found", file_size, found_at);
@@ -59,6 +77,7 @@ string readFile(fs::path filepath, size_t offset) {
 
   stringstream content;
   content << file.rdbuf();
+  file.close();
   string result = content.str();
 
   //fmt::print("File content: {}\n", content.str()); 
@@ -88,17 +107,50 @@ string readFile(fs::path filepath, size_t offset) {
 string findSoundTag(string file) {
   int start = 0;
   int end = 0;
-  string result = "";
+  string soundTag = "";
   for (int i = 0; i < file.length(); i++) {
     if (file[i] == '[')
       start = i;
+      //end = i;
     if (file[i] == ']')
       end = i;
+      //start = i;
   }
 
   if (start != 0 && end != 0) {
-    result = file.substr(start, end);
+    soundTag = file.substr(start, end);
   }
+
+  string result = "";
+  //result.reserve(100); // Max tag length
+  result.reserve(50);
+  //const regex regexp(R"(\w)(_)?");
+  //const regex regexp("(\\w)(_)?");
+  //regex regexp("(\\w)(_)?");
+  //regex exp("(\\w)(_)?");
+  regex exp("(\\w)");
+
+  //if ( regex_match ( regexp.begin(), regexp.end(), soundTag ) )
+    //result.append();
+
+  smatch match;
+  //if (regex_search(soundTag.begin(), soundTag.end(), match, regexp))
+  //if (regex_search(begin(soundTag), end(soundTag), match, regexp))
+  //if (regex_search(soundTag.begin(), soundTag.end(), match, exp))
+  for (int i = 0; i < soundTag.length(); i++) {
+  //if (regex_match(soundTag[i], match, exp)) 
+  if (regex_search(soundTag, match, exp))
+    result += match[i];
+  }
+  //for (const auto &tag : soundTag) { 
+    ////if (regex_match(tag, match, regexp)) {
+    ////if (regex_search(tag, match, regexp)) {
+    //if (regex_match(soundTag, match, regexp)) {
+      //result += match.str();
+    //}
+  //}
+
+  
   return result;
 }
 
@@ -115,19 +167,32 @@ int extract(fs::path filepath) {
   if (isCorrupted(filepath, file)) { throw exception(); }
 
   size_t audioOffset = getAudioOffset(file);
+  //file.close();
 
-  assert(audioOffset != -1);
-  assert(audioOffset != 0);
+  //assert(audioOffset != -1);
+  //assert(audioOffset != 0);
   fmt::print("Audio File offset: \t\t{} \tbytes \n\n", audioOffset); 
 
 
+  file.seekg(audioOffset, ios::beg);
+
+  fs::path tempfilepath = "temp.ogg";
+  ofstream tempFile(tempfilepath, ifstream::out | ifstream::binary);
+  tempFile << file.rdbuf();
+  file.close();
+
+  //tempFile << soundTagStem;
+  tempFile.close();
+  //fs::path tempfilepath = "temp.ogg";
+  //ofstream tempFile();
+
   //fs::path tempfilepath = "temp.ogg";
 
-  string audioContent = readFile(filepath, audioOffset);
   //ofstream outputFile(tempfilepath, ifstream::out | ifstream::binary);
   //outputFile << audioContent;
   //outputFile.close();
   //fmt::print("audioContent string: {}\n", audioContent);
+  string audioContent = readFile(filepath, audioOffset);
   assert(!audioContent.empty());
 
   //string soundTag = fmt::format("{}.ogg", findSoundTag(audioContent)); 
@@ -169,13 +234,14 @@ int extract(fs::path filepath) {
   assert(!soundTag.empty());
   //fmt::print("Sound tag: \t\t{}\n", soundTag);
   //cerr << soundTag << endl;
+  //ofstream audioFile("audio01.ogg", ifstream::out | ifstream::binary); 
   ofstream audioFile(soundTag.c_str(), ifstream::out | ifstream::binary); 
 
   //audioFile.write(audioContent.c_str(), sizeof(char)*audioContent.size());
   audioFile.write(audioContent.c_str(), audioContent.length());
   //audioFile << audioContent;
 
-  file.close();
+  //file.close();
   audioFile.close();
 
    // Filter file 
