4c0cb95149f5de8861a7e1c7db3af7888f1f7128
Joseph Diza <josephm.diza@gmail.com>
2020-12-25 02:59:12 -0800

    Add arg checking in app/extract.cpp

--- app/extract.cpp
+++ app/extract.cpp
@@ -7,14 +7,11 @@
 using namespace std;
 namespace fs = std::filesystem;
 
-int main(int argc, char** argv) {
-  //string path;
-  //if (argc > 0 && argc < 2) 
-    //std::string path = argv[1];
-
-  //fs::path filepath = argv[0];
-  //fs::path filepath = path;
-  fs::path filepath = argv[1];
+int main(int argc, char** argv) { 
+  fs::path filepath = "";
+  if (!string(argv[1]).empty())
+    filepath = argv[1];
+    //fs::path filepath = argv[1];
   extract(filepath);
   return 0;
 }
