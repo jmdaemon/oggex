d8f81e4c68bd041056732635d46dca9c2821020c
Joseph Diza <josephm.diza@gmail.com>
2020-12-25 17:17:18 -0800

    Remove unneeded imports in EmbedAudio unit tests

--- test/audio/EmbedAudioTests.cpp
+++ test/audio/EmbedAudioTests.cpp
@@ -4,15 +4,8 @@
 #include "File.h"
 #include "Image.h"
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include <filesystem>
-#include <map>
-
 using namespace std;
 namespace fs = std::filesystem;
-
 
 TEST_CASE("Audio files can be embedded into image files") {
   SUBCASE("Test toLowerCase()") {
@@ -23,17 +16,6 @@ TEST_CASE("Audio files can be embedded into image files") {
     CHECK(File::toLowerCase(".JPEG") == ".jpeg");
     CHECK(File::toLowerCase(".PNG") == ".png");
   }
-
-
-  //SUBCASE("Running image file checks") {
-    //std::filesystem::path filepath = "../../inputFile1.png"; 
-    //ifstream file(filepath, ifstream::in | ifstream::binary);
-    //CHECK(isImage(filepath));
-    //CHECK(imageUnder4MiB(std::filesystem::file_size(filepath)));
-    //REQUIRE(!isCorrupted(filepath, file));
-    ////REQUIRE(imageNotCorrupted(filepath) == true);
-  //}
-
   //SUBCASE("") {
   //}
 }
