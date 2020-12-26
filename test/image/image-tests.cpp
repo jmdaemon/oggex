229da12315a0dfd2629bb946d4086ae966140937
Joseph Diza <josephm.diza@gmail.com>
2020-12-25 17:21:28 -0800

    Move image tests into image-tests.cpp

--- test/image/image-tests.cpp
+++ test/image/image-tests.cpp
@@ -5,6 +5,10 @@
 #include <string>
 
 #include "Image.h"
+#include "FileType.tpp"
+
+using namespace std;
+namespace fs = std::filesystem; 
 
 std::string readImage() {
   std::string filepath = ("../../resources/12727a5f30b1429a1fd35113fd2550d8.png");
@@ -17,4 +21,14 @@ std::string readImage() {
 
 TEST_CASE("Testing Image class") { 
   CHECK(readImage() == "Successfully read file");
+
+  SUBCASE("Running image file checks") {
+    std::filesystem::path filepath = "../../inputFile1.png"; 
+    ifstream file(filepath, ifstream::in | ifstream::binary);
+    CHECK(Image::isImage(filepath));
+    CHECK(Image::imageUnder4MiB(std::filesystem::file_size(filepath)));
+    REQUIRE(!isCorrupted(filepath, file));
+    //REQUIRE(imageNotCorrupted(filepath) == true);
+  }
+
 }
