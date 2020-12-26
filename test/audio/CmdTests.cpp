aa5729fc8903af3f369838326ec2801ccf77f766
Joseph Diza <josephm.diza@gmail.com>
2020-12-25 17:15:46 -0800

    Move cli parsing tests from EmbedAudio into CmdTests

---
+++ test/audio/CmdTests.cpp
@@ -0,0 +1,45 @@
+#include "../doctest-main.cpp"
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <map>
+
+#include "Cmd.h"
+
+using namespace std;
+namespace fs = std::filesystem;
+
+bool quality;
+
+template <typename Map>
+bool key_compare (Map const &lhs, Map const &rhs) {
+    return lhs.size() == rhs.size()
+        && std::equal(lhs.begin(), lhs.end(), rhs.begin(), 
+                      [] (auto a, auto b) { return a.first == b.first; });
+}
+
+map<int, string> createArgvInput(vector<string> arguments) {
+    vector<string> firstTestArgs = {""};
+    vector<char*> argv;
+    for (const auto& arg : arguments)
+        argv.push_back((char*)arg.data()); argv.push_back(nullptr);
+
+    map<int, string> filepaths = parseOptions(argv.size() - 1, argv.data(), quality); 
+    return filepaths;
+}
+
+TEST_CASE("Command line arguments can be passed into embed") {
+  SUBCASE("Testing parseOptions()") { 
+    INFO("Current output of parseOptions: ");
+
+    vector<string> firstTestArgs = {""};
+    CHECK_THROWS_AS(createArgvInput(firstTestArgs), std::exception);
+
+    vector<string> secondTestArgs = {"image.png", "audio.ogg"};
+    
+    map<int, string> expectedPaths = { {0, "image.png"}, {1, "audio.ogg"}};
+    CHECK(key_compare(expectedPaths, createArgvInput(secondTestArgs)));
+  } 
+}
--- test/audio/EmbedAudioTests.cpp
+++ test/audio/EmbedAudioTests.cpp
@@ -13,23 +13,6 @@
 using namespace std;
 namespace fs = std::filesystem;
 
-//template <typename Map>
-
-//bool key_compare (Map const &lhs, Map const &rhs) {
-    //return lhs.size() == rhs.size()
-        //&& std::equal(lhs.begin(), lhs.end(), rhs.begin(), 
-                      //[] (auto a, auto b) { return a.first == b.first; });
-//}
-
-//map<int, string> createArgvInput(vector<string> arguments) {
-    //vector<string> firstTestArgs = {""};
-    //vector<char*> argv;
-    //for (const auto& arg : arguments)
-        //argv.push_back((char*)arg.data()); argv.push_back(nullptr);
-
-    //map<int, string> filepaths = parseOptions(argv.size() - 1, argv.data()); 
-    //return filepaths;
-//}
 
 TEST_CASE("Audio files can be embedded into image files") {
   SUBCASE("Test toLowerCase()") {
@@ -41,17 +24,6 @@ TEST_CASE("Audio files can be embedded into image files") {
     CHECK(File::toLowerCase(".PNG") == ".png");
   }
 
-  //SUBCASE("Testing parseOptions()") { 
-    //INFO("Current output of parseOptions: ");
-
-    //vector<string> firstTestArgs = {""};
-    //CHECK_THROWS_AS(createArgvInput(firstTestArgs), std::exception);
-
-    //vector<string> secondTestArgs = {"image.png", "audio.ogg"};
-    
-    //map<int, string> expectedPaths = { {0, "image.png"}, {1, "audio.ogg"}};
-    //CHECK(key_compare(expectedPaths, createArgvInput(secondTestArgs)));
-  //} 
 
   //SUBCASE("Running image file checks") {
     //std::filesystem::path filepath = "../../inputFile1.png"; 
