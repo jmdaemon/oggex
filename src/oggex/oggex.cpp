76f66ed405f67d3c4f9c53965239c742cb6e4b13
Joseph Diza <josephm.diza@gmail.com>
2020-12-28 17:49:27 -0800

    Show dashboard ui at application start

--- src/oggex/oggex.cpp
+++ src/oggex/oggex.cpp
@@ -1,32 +1,48 @@
+//#include "Dashboard.h"
+
 #include <iostream>
 #include <string>
 
 #include <gtkmm.h>
-#include <gtkmm/label.h>
-//#include <gtkmm/main.h>
-//#include <gtkmm/builder.h>
+//#include <gtkmm/label.h>
+
+#include <gtkmm/application.h>
+#include <gtkmm/builder.h>
+#include <gtkmm/main.h>
+
 //#include <gtkmm/window.h>
 
 using namespace std;
 
 namespace { 
-  string dashboard = "../../Dashboard.glade";
-  Glib::ustring dboard = "Dashboard";
-  Glib::ustring browseImageID = "browseImage";
-  Gtk::Button* pButton = nullptr;
+  const string DASHBOARD_UI = "../../Dashboard.glade";
 
+  Glib::ustring dashboardID   = "Dashboard";
+  Glib::ustring browseImageID = "browseImage";
+  Glib::ustring fileChooserID = "selectFile";
+  //Glib::ustring gtkOverlayID  = "browseFiles";
+
+  //Gtk::FileChooserWidget* pFileWidget = nullptr;
+  Gtk::Button* pBrowseImageButton = nullptr;
   Gtk::Window* pWindow = nullptr;
+
   Glib::RefPtr<Gtk::Application> app;
 
-  void on_button_clicked() {
-  if (pWindow)
-    pWindow->hide();
+  //void on_button_clicked() { 
+    //if (pWindow) 
+      //pWindow->hide();
+  //}
+  void on_button_clicked() { 
+    //if (pFileWidget)
+      //pFileWidget->show();
+    if (pWindow) 
+      pWindow->hide();
   }
 
 void on_app_activate() {
   auto refBuilder = Gtk::Builder::create();
   try { 
-    refBuilder->add_from_file(dashboard);
+    refBuilder->add_from_file(DASHBOARD_UI);
     } catch(const Glib::FileError& ex) { 
       std::cerr << "FileError: " << ex.what() << std::endl; 
       return; 
@@ -39,17 +55,17 @@ void on_app_activate() {
     }
 
   //pWindow = refBuilder->get_widget<Gtk::Window>(dboard, pWindow);
-  refBuilder->get_widget<Gtk::Window>(dboard, pWindow);
+  refBuilder->get_widget<Gtk::Window>(dashboardID, pWindow);
   if (!pWindow) {
     std::cerr << "Could not get the window" << std::endl;
-    return;
-    }
+    return; 
+  }
 
-  //auto pButton = refBuilder->get_widget<Gtk::Button>(browseImageID, browseImage);
-  //auto pButton = refBuilder->get_widget<Gtk::Button>(browseImageID, browseImage);
-  refBuilder->get_widget<Gtk::Button>(browseImageID, pButton);
-  if (pButton)
-    pButton->signal_clicked().connect([] () { on_button_clicked(); });
+  //auto pBrowseImageButton = refBuilder->get_widget<Gtk::Button>(browseImageID, browseImage);
+  //auto pBrowseImageButton = refBuilder->get_widget<Gtk::Button>(browseImageID, browseImage);
+  refBuilder->get_widget<Gtk::Button>(browseImageID, pBrowseImageButton);
+  if (pBrowseImageButton)
+    pBrowseImageButton->signal_clicked().connect([] () { on_button_clicked(); });
 
   pWindow->signal_hide().connect([] () { delete pWindow; });
 
@@ -60,13 +76,22 @@ void on_app_activate() {
 
 
 int main(int argc, char *argv[]) {
-  //string dashboard = "Dashboard.glade";
+  string dashboard = "Dashboard.glade";
   //auto builder = Gtk::Builder::create_from_file(dashboard);
   //auto pWindow = refBuilder->get_widget<Gtk::Window>("Dashboard");
 
   //auto pWindow = builder->get_widget<Gtk::Window>("Dashboard");
   //app = Gtk::Application::create("oggex");
   //app->signal_activate().connect([] () { on_app_activate(); });
+  //auto app = Gtk::Application::create("oggex");
+  //app->signal_activate().connect([] () { on_app_activate(); });
+  //return app->run(argc, argv);
+  //auto builder = Gtk::Builder::create_from_file(dashboard);
+  //return app->make_window_and_run<DashboardController::DashboardController>(argc, argv);
+  //return app->run(DashboardController::DashboardController(), argc, argv);
+  //return builder->make_window_and_run<DashboardController>(argc, argv);
+  //DashboardController con;
+  //return app->run(con, argc, argv);
   app = Gtk::Application::create("oggex");
   app->signal_activate().connect([] () { on_app_activate(); });
   return app->run(argc, argv);
