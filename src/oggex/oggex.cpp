b462398e05e0178495457b386e4988c36c99ee14
Joseph Diza <josephm.diza@gmail.com>
2020-12-28 19:36:29 -0800

    Set audioQuality spinbutton and adjustment

--- src/oggex/oggex.cpp
+++ src/oggex/oggex.cpp
@@ -15,16 +15,27 @@ namespace {
 
   Glib::ustring dashboardID   = "Dashboard";
   Glib::ustring browseImageID = "browseImage";
+  Glib::ustring addSoundID    = "addSound";
   Glib::ustring fileChooserID = "selectFile";
+  Glib::ustring setQualityID  = "quality";
+  Glib::ustring qualityAdjustmentID  = "qualityAdjustment";
 
   Gtk::Button* pBrowseImageButton = nullptr;
+  Gtk::Button* pAddSoundButton = nullptr;
+  Gtk::SpinButton* pSetQualitySpinButton = nullptr;
+  //Gtk::Adjustment* qualityAdjustment = nullptr;
   Gtk::Window* pWindow = nullptr;
 
   Glib::RefPtr<Gtk::Application> app;
+  Glib::RefPtr<Gtk::Adjustment> qualityAdjustment;
 
   void on_button_clicked() { 
     if (pWindow) 
       pWindow->hide();
+  }
+
+  void onQualityChange() { 
+    pSetQualitySpinButton->set_digits( pSetQualitySpinButton->get_value_as_int() );
   }
 
 void on_app_activate() {
@@ -49,8 +60,22 @@ void on_app_activate() {
   }
 
   refBuilder->get_widget<Gtk::Button>(browseImageID, pBrowseImageButton);
+  refBuilder->get_widget<Gtk::Button>(addSoundID, pAddSoundButton);
+
+  refBuilder->get_widget<Gtk::SpinButton>(setQualityID, pSetQualitySpinButton); 
+  //qualityAdjustment = refBuilder->get_object<Gtk::Adjustment>(qualityAdjustmentID); 
+  //qualityAdjustment = refBuilder->get_object<Gtk::Adjustment>(qualityAdjustmentID); 
+  //qualityAdjustment = static_cast<Gtk::Adjustment>(refBuilder->get_object(qualityAdjustmentID)); 
+  //qualityAdjustment = Gtk::Adjustment::create(4.0, 0.0, 10.0, 1, 10, 0);
+  qualityAdjustment = pSetQualitySpinButton->get_adjustment();
+  //pSetQualitySpinButton(qualityAdjustment);
   if (pBrowseImageButton)
     pBrowseImageButton->signal_clicked().connect([] () { on_button_clicked(); });
+  if (pAddSoundButton)
+    pAddSoundButton->signal_clicked().connect([] () { on_button_clicked(); }); 
+  if (qualityAdjustment)
+    qualityAdjustment->signal_value_changed().connect([] () { onQualityChange(); });
+    //qualityAdjustment->signal_value_changed().connect( sigc::mem_fun(*this, onQualityChange) );
 
   pWindow->signal_hide().connect([] () { delete pWindow; });
 
