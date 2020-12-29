6ac364e850e0f224578f446e3c3cad9b96596083
Joseph Diza <josephm.diza@gmail.com>
2020-12-29 10:11:37 -0800

    Remove dead code in Dashboard header

--- include/ui/Dashboard.h
+++ include/ui/Dashboard.h
@@ -2,15 +2,7 @@
 #define DASHBOARD_H
 
 #include <gtkmm.h>
-//#include <gtkmm/window.h>
-////#include <gtkmm/overlay.h>
-////#include <gtkmm/filechooser.h>
-//#include <gtkmm/entry.h>
-//#include <gtkmm/button.h>
-//#include <gtkmm/spinbutton.h>
-//#include <gtkmm/progressbar.h>
 
-//class DashboardController : public Gtk::Window {
 class DashboardWindow;
 class DashboardController : public Gtk::Application {
   private:
@@ -26,44 +18,6 @@ class DashboardController : public Gtk::Application {
 
   public:
     static Glib::RefPtr<DashboardController> create();
-  //protected: 
-    //Gtk::Entry imageFilePath;
-    //Gtk::Entry audioFilePath;
-
-    //Gtk::Button browseImage;
-    //Gtk::Button addSound;
-    //Gtk::Button readSound;
-    //Gtk::Button reset;
-
-    //Gtk::Entry outputFileName;
-    //Gtk::SpinButton quality;
-    //Gtk::Button browseSaveOutput;
-    //Gtk::Button embed;
-    //Gtk::Button extract;
-
-    ////GtkOverlay* browseFiles;
-    ////GtkFileChooser* selectFile;
-    //Gtk::ProgressBar progressBar;
-
-    ////Signal handlers 
-    //void on_button_file_clicked();
-    //void on_button_folder_clicked();
-    //void on_file_dialog_response(int response_id, Gtk::FileChooserDialog* dialog);
-    //void on_folder_dialog_response(int response_id, Gtk::FileChooserDialog* dialog);
-
-    //void clickToBrowse();
-    ////void addSoundFile();
-    ////void removeSoundFile();
-    ////void readSoundFile();
-
-    ////void setQuality();
-    ////void embedAudio();
-    ////void extractAudio();
-
-  //public:
-    //DashboardController();
-    //DashboardController(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& refGlade);
-    //virtual ~DashboardController();
 };
 
 #endif
