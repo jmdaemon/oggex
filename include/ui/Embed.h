a8e27d5b9a403f9e492830108a787054a1f8a3f6
Joseph Diza <josephm.diza@gmail.com>
2021-01-24 11:38:29 -0800

    Change EmbedController -> Embed

--- include/ui/Embed.h
+++ include/ui/Embed.h
@@ -4,19 +4,19 @@
 #include <gtkmm.h>
 
 class EmbedWindow;
-class EmbedController : public Gtk::Application {
+class Embed : public Gtk::Application {
   private:
     EmbedWindow* create_appwindow();
     void on_hide_window(Gtk::Window* window);
 
   protected:
-    EmbedController();
+    Embed();
 
     void on_activate() override;
 
   public:
     //EmbedWindow* create_appwindow();
-    static Glib::RefPtr<EmbedController> create();
+    static Glib::RefPtr<Embed> create();
 };
 
 #endif 
