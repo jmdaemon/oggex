4241baada8504fde0cabedd7bb6b960fd24f6dc4
Joseph Diza <josephm.diza@gmail.com>
2021-06-20 15:54:47 -0700

    Remove isImage forward declaration

--- include/util/Image.h
+++ include/util/Image.h
@@ -11,11 +11,11 @@ namespace Image {
   class ImageData : public File::File {
     private: 
       std::map<int, std::string> FileExtensions = { 
-        {0, ".jpg"},
-        {1, ".jpeg"},
-        {2, ".gif"},
-        {3, ".png"},
-        {4, ".webm"}, 
+        { 0, ".jpg"},
+        { 1, ".jpeg"},
+        { 2, ".gif"},
+        { 3, ".png"},
+        { 4, ".webm"},
       };
 
       std::string imagePath;
@@ -28,12 +28,10 @@ namespace Image {
       virtual bool isValid(std::filesystem::path filepath);
 
       std::filesystem::path createOutputFilename();
-
       std::string getImage() { return this->imagePath; }
       void setImage(std::string imagePath) { this->imagePath = imagePath; }
   };
-  bool isImage(std::string file);
-  bool isImage(std::filesystem::path filepath);
+
   bool imageUnder4MiB (std::filesystem::path imagePath); 
 }
 
