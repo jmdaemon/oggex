a7638ae11a1aee8f30c33bf405717bdfd669630d
Joseph Diza <josephm.diza@gmail.com>
2022-03-29 14:49:09 -0700

    Create initial TreeView prototype for model

--- app/qt/embedwidget/embedwidget.cpp
+++ app/qt/embedwidget/embedwidget.cpp
@@ -1,9 +1,34 @@
 #include "embedwidget.h"
 #include "ui_embedwidget.h"
 
+#include <QStandardItemModel>
+#include <QStandardItem>
+
 EmbedWidget::EmbedWidget(QWidget *parent)
     : QWidget(parent), ui(new Ui::EmbedWidget) {
     ui->setupUi(this);
+    QStandardItemModel* model = new QStandardItemModel();
+
+    QStandardItem* item1 = new QStandardItem(QIcon("test.png"), "ImageFile1.png");
+    QStandardItem* item2 = new QStandardItem(QIcon("test.png"), "SoundFile1.ogg");
+    QStandardItem* item3 = new QStandardItem(QIcon("test.png"), "SoundFile2.ogg");
+    QStandardItem* item4 = new QStandardItem("OutputFile1.png");
+
+    QList<QStandardItem*> outputs = { item4 };
+    // Set up the hierarchy as:
+    // First Column:
+    // ImageFile1
+    //  - Sound File1
+    //  - Sound File2
+    // Second Column
+    //  - Sound File2
+
+    model->appendRow(item1);
+    item1->appendRow(item2);
+    item1->appendRow(item3);
+    model->appendColumn(outputs);
+
+    ui->treeView->setModel(model);
 }
 
 EmbedWidget::~EmbedWidget() {
--- app/qt/mainwindow/mainwindow.cpp
+++ app/qt/mainwindow/mainwindow.cpp
@@ -1,8 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-#include <QStackedWidget>
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
@@ -11,15 +9,6 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->quitButton, SIGNAL(clicked()), this, SLOT(close()));
 
     // Navigate to embedwindow when embedButton is clicked
-    //connect(ui->embedButton, SIGNAL(clicked()), this, SLOT(activateWindow());
-    //connect(ui->embedButton, SIGNAL(clicked()), ui->stackedWidget, SLOT(&QStackedWidget::setCurrentIndex(0)));
-    //connect(ui->embedButton, SIGNAL(clicked()), ui->stackedWidget, [this]{ setCurrentIndex(0);});
-    //connect(ui->embedButton, SIGNAL(clicked()), ui->stackedWidget, { QStackedWidget::setCurrentIndex(0);});
-    //connect(ui->embedButton, SIGNAL(clicked()), ui->stackedWidget, SLOT(setCurrentIndex(0)));
-    //connect(ui->embedButton, SIGNAL(clicked()), ui->stackedWidget, SLOT(setCurrentIndex("page")));
-    //connect(ui->embedButton, SIGNAL(clicked()), ui->stackedWidget, SLOT(setCurrentWidget(ui->widget)));
-    //connect(ui->embedButton, SIGNAL(clicked()), ui->stackedWidget.setCurrentIndex(0));
-    //QStackedWidget::setCurrentIndex();
     connect(ui->embedButton, &QPushButton::clicked, this, [=]() {
         // Show embedPage
         ui->stackedWidget->setCurrentIndex(0);
