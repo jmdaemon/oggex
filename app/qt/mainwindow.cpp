a78d6a8bbaaf5032973c858d005443c23dc083eb
Joseph Diza <josephm.diza@gmail.com>
2022-03-29 11:54:08 -0700

    Close window when quitButton is clicked

--- app/qt/mainwindow.cpp
+++ app/qt/mainwindow.cpp
@@ -1,10 +1,26 @@
 #include "mainwindow.h"
 #include "./ui/ui_mainwindow.h"
 
+#include <QObject>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent) , ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    //ui->QuitButton
+    //QObject::connect(ui->quitButton,
+            //&QPushButton::clicked, &ui, &QCoreApplication::quit, Qt::QueuedConnection);
+    //QObject::connect(ui->quitButton, &QPushButton::clicked, nullptr, &QCoreApplication::quit, Qt::QueuedConnection);
+    //QObject::connect(ui->quitButton, &QPushButton::clicked, nullptr, &QCoreApplication::quit, Qt::QueuedConnection);
+    //QObject::connect(const QObject *asender, const char *asignal, const char *amember)
+    //ui->quitButton->.
+    //connect(ui->quitButton, SIGNAL(triggered()), this, SLOT(MainWindow::clickQuitButton()));
+    //connect(ui->quitButton, SIGNAL(triggered()), this, SLOT(this->close()));
+    connect(ui->quitButton, SIGNAL(clicked()), this, SLOT(close()));
 }
+
+//void MainWindow::clickQuitButton() {
+    //this->close();
+//}
 
 MainWindow::~MainWindow() {
     delete ui;
--- app/qt/mainwindow.h
+++ app/qt/mainwindow.h
@@ -3,6 +3,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+//#include <QPushButton>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -12,6 +13,9 @@ class MainWindow : public QMainWindow {
     Q_OBJECT
 private:
     Ui::MainWindow *ui;
+    //QPushButton *quitButton;
+//private slots:
+  //void clickQuitButton();
 
 public:
     MainWindow(QWidget *parent = nullptr);
