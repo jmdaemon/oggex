a74d0881a58c7434b479cea618a9dbd65c7ed255
Joseph Diza <josephm.diza@gmail.com>
2022-03-29 14:55:01 -0700

    Set column titles in TreeView

--- app/qt/embedwidget/embedwidget.cpp
+++ app/qt/embedwidget/embedwidget.cpp
@@ -14,7 +14,7 @@ EmbedWidget::EmbedWidget(QWidget *parent)
     QStandardItem* item3 = new QStandardItem(QIcon("test.png"), "SoundFile2.ogg");
     QStandardItem* item4 = new QStandardItem("OutputFile1.png");
 
-    QList<QStandardItem*> outputs = { item4 };
+    QList<QStandardItem*> outputsList = { item4 };
     // Set up the hierarchy as:
     // First Column:
     // ImageFile1
@@ -26,7 +26,18 @@ EmbedWidget::EmbedWidget(QWidget *parent)
     model->appendRow(item1);
     item1->appendRow(item2);
     item1->appendRow(item3);
-    model->appendColumn(outputs);
+    model->appendColumn(outputsList);
+
+    //QStandardItem* inputs = new QStandardItem("Inputs");
+    //QStandardItem* outputs = new QStandardItem("Outputs");
+
+    //model->horizontalHeaderItem(0)->setText(tr("Inputs"));
+    //model->horizontalHeaderItem(1)->setText(tr("Output"));
+    //model->horizontalHeaderItem(0, inputs);
+    //model->horizontalHeaderItem(1)->setText(tr("Output"));
+    model->setHeaderData(0, Qt::Horizontal, "Inputs", Qt::DisplayRole );
+    model->setHeaderData(1, Qt::Horizontal, "Outputs", Qt::DisplayRole );
+
 
     ui->treeView->setModel(model);
 }
