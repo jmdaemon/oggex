08226df670523a9ebe2a6cf7aa86ab854bdf169f
Joseph Diza <josephm.diza@gmail.com>
2022-03-31 11:10:20 -0700

    Add functions to add{Image, Sound}s to the model

--- app/qt/embedwidget/embedwidget.cpp
+++ app/qt/embedwidget/embedwidget.cpp
@@ -1,40 +1,39 @@
 #include "embedwidget.h"
-//#include "soundbutton.h"
 #include "ui_embedwidget.h"
 
-#include <QStandardItemModel>
-#include <QStandardItem>
-#include <QPushButton>
-#include <QTreeWidgetItem>
-
-
-//class SoundButton : public QPushButton, public QStandardItem {
-    //Q_OBJECT
-//public:
-        //explicit QLabelClickable( const QString& text="", QWidget* parent=0 );
-    //~QLabelClickable();
-//signals:
-    //void clicked(int, int);
-//protected:
-    //void mousePressEvent(QMouseEvent* event);
-
-//};
+//#include <QPushButton>
+//#include <QTreeWidgetItem>
 
 EmbedWidget::EmbedWidget(QWidget *parent)
     : QWidget(parent), ui(new Ui::EmbedWidget) {
     ui->setupUi(this);
+
     QStandardItemModel* model = new QStandardItemModel();
-    //QTreeWidgetItem* item = new QTreeWidgetItem(QPushButton(""));
 
-    QStandardItem* item1 = new QStandardItem(QIcon("test.png"), "ImageFile1.png");
-    QStandardItem* item2 = new QStandardItem(QIcon("test.png"), "SoundFile1.ogg");
-    QStandardItem* item3 = new QStandardItem(QIcon("test.png"), "SoundFile2.ogg");
-    QStandardItem* soundButton = new QStandardItem("Add Sound");
-    QStandardItem* item4 = new QStandardItem("OutputFile1.png");
-    //QStandardItem* addImage = new QStandardItem(QIcon("test.png"), QPushButton("Add Image"));
-    QStandardItem* addImage = new QStandardItem(QIcon("test.png"), "Add Image");
+    auto image  = makeEntry("ImageFile1.png");
+    auto sound1 = makeEntry("SoundFile1.ogg");
+    auto sound2 = makeEntry("SoundFile2.ogg");
+    auto sb     = makeSoundButton();
+    auto output = makeEntry("OutputFile1.png");
 
-    QList<QStandardItem*> outputsList = { item4 };
+    QList<QStandardItem*> outputsList = { output };
+
+    addSound(sound1, image);
+    addSound(sound2, image);
+    addSound(sb, image);
+    addImage(image, model);
+
+    model->appendColumn(outputsList);
+
+    //QStandardItem* item1 = new QStandardItem(QIcon("test.png"), "ImageFile1.png");
+    //QStandardItem* item2 = new QStandardItem(QIcon("test.png"), "SoundFile1.ogg");
+    //QStandardItem* item3 = new QStandardItem(QIcon("test.png"), "SoundFile2.ogg");
+    //QStandardItem* soundButton = new QStandardItem("Add Sound");
+    //QStandardItem* item4 = new QStandardItem("OutputFile1.png");
+
+    //QStandardItem* addImage = new QStandardItem(QIcon("test.png"), "Add Image");
+
+    //QList<QStandardItem*> outputsList = { item4 };
     // Set up the hierarchy as:
     // First Column:
     // ImageFile1
@@ -43,57 +42,51 @@ EmbedWidget::EmbedWidget(QWidget *parent)
     // Second Column
     //  - Sound File2
 
-    model->appendRow(item1);
-    item1->appendRow(item2);
-    item1->appendRow(item3);
-    item1->appendRow(soundButton);
-    model->appendColumn(outputsList);
+    //model->appendRow(item1);
+    //item1->appendRow(item2);
+    //item1->appendRow(item3);
+    //item1->appendRow(soundButton);
+    //model->appendColumn(outputsList);
 
     // Sets the Column Headers
     model->setHeaderData(0, Qt::Horizontal, "Inputs", Qt::DisplayRole );
     model->setHeaderData(1, Qt::Horizontal, "Outputs", Qt::DisplayRole );
 
-    // Add Soundbutton to the model
-    //SoundButton sb;
-    //model->appendRow(&sb);
-
-
-    // Create one [Add Sound] Button
-    //QWidget* addSoundWidget = new QWidget();
-    //QPushButton* addSoundButton = new QPushButton();
-    //addSoundButton->setText("Add Sound");
-
-    //QHBoxLayout* pLayout = new QHBoxLayout(addSoundWidget);
-    //pLayout->addWidget(addSoundButton);
-    //pLayout->setAlignment(Qt::AlignCenter);
-    //pLayout->setContentsMargins(0, 0, 0, 0);
-    //addSoundWidget->setLayout(pLayout);
-
-    ////ui->treeView->QAbstractItemView::setModel
-    //model->appendRow(addSoundWidget);
-    //model->setCellWidget(addSoundWidget);
-    //ui->treeView->setCellWidget(0, 3, pWidget);
-
     // Set the current model
     ui->treeView->setModel(model);
 }
-
-// Creates an "Add Sound" button
-QStandardItem* addSound() {
+// Creates an "Add Sound" entry
+QStandardItem* EmbedWidget::makeSoundButton() {
     QStandardItem* soundButton = new QStandardItem("Add Sound");
     return soundButton;
 }
 
-// Adds the image path to the tree hierarchy alongside an "Add Sound" button
-QStandardItem* addImage(QString path) {
+// Creates the image entry in the model
+QStandardItem* EmbedWidget::makeEntry(QString path) {
     QStandardItem* image = new QStandardItem(path);
     return image;
 }
 
+// Creates the image entry in the model
+//QStandardItem* EmbedWidget::makeImage(QString path) {
+    //QStandardItem* image = new QStandardItem(path);
+    //return image;
+//}
+
+// Adds the sound to the image
+void EmbedWidget::addSound(QStandardItem* sound, QStandardItem* image) {
+    image->appendRow(sound);
+}
+
+// Adds the image path to the model
+void EmbedWidget::addImage(QStandardItem* image, QStandardItemModel* model) {
+    model->appendRow(image);
+}
+
 // Adds a new image with an "Add Sound" button to our model
-void addToModel(QString path, QStandardItemModel* model) {
-    QStandardItem* image = addImage(path);
-    QStandardItem* soundButton = addSound();
+void EmbedWidget::addToModel(QString path, QStandardItemModel* model) {
+    QStandardItem* image = makeEntry(path);
+    QStandardItem* soundButton = makeSoundButton();
     image->appendRow(soundButton);
     model->appendRow(image);
 }
--- app/qt/embedwidget/embedwidget.h
+++ app/qt/embedwidget/embedwidget.h
@@ -2,18 +2,27 @@
 #define EMBEDWIDGET_H
 
 #include <QWidget>
+#include <QStandardItem>
+#include <QStandardItemModel>
+#include <QString>
 
 namespace Ui { class EmbedWidget; }
 
 class EmbedWidget : public QWidget {
     Q_OBJECT
 
+private:
+    Ui::EmbedWidget *ui;
+
 public:
     explicit EmbedWidget(QWidget *parent = nullptr);
     ~EmbedWidget();
-
-private:
-    Ui::EmbedWidget *ui;
+    //QStandardItem* makeSound();
+    QStandardItem* makeSoundButton();
+    //QStandardItem* makeImage(QString path);
+    QStandardItem* makeEntry(QString path);
+    void addSound(QStandardItem* sound, QStandardItem* image);
+    void addImage(QStandardItem* image, QStandardItemModel* model);
+    void addToModel(QString path, QStandardItemModel* model);
 };
-
 #endif // EMBEDWIDGET_H
