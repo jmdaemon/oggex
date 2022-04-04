2514e30d2c816dbb9e7c7214e876342dc42b720e
Joseph Diza <josephm.diza@gmail.com>
2022-04-04 00:08:37 -0700

    Add under_limit function

--- src/utility/file.c
+++ src/utility/file.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 /**
@@ -23,7 +24,7 @@ bool file_exists(FILE* fp) {
   * path: The file path as a character array
   * returns: The size of the file on disk
   */
-long int filesize(char* path) {
+long int file_size(char* path) {
   FILE* fp = fopen(path, "r");
 
   if (!file_exists(fp))
@@ -36,4 +37,16 @@ long int filesize(char* path) {
   // closing the file
   fclose(fp);
   return res;
+}
+
+/**
+  * Checks if a file is below a set size limit
+  * path: The file path
+  * size: The upper bound size limit
+  * returns: True if the file is less than or equal to the upper bound and False otherwise
+  */
+bool under_limit(char* path, size_t size) {
+  size_t filesize = file_size(path);
+  bool result = (filesize <= size) ? true : false;
+  return result;
 }
