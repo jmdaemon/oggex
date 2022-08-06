a084683db6870702fada03af09e85989228ffbfe
Joseph Diza <josephm.diza@gmail.com>
2022-08-05 21:28:30 -0700

    Prototype simpler data structures

---
+++ include/media.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef OGGEX_MEDIA_H
+#define OGGEX_MEDIA_H
+
+/** Sound file */
+struct Sound {
+  char* src;  /* Sound file path */
+  char* dest; /* Image name */
+  char* temp;
+  char* log;
+};
+
+/** Audio Settings */
+struct Settings {
+  int quality;
+  bool mono_channel;
+};
+
+struct Media {
+  Sound sound;
+  Settings settings;
+  Options options{};
+};
+
+#endif // OGGEX_MEDIA_H
