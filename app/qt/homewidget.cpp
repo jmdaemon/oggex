74314e38737f3e29e1adaa425edf792ecfc8b924
Joseph Diza <josephm.diza@gmail.com>
2022-08-11 16:35:53 -0700

    Add HomeWidget component

---
+++ app/qt/homewidget.cpp
@@ -0,0 +1,24 @@
+#include "homewidget.h"
+#include "ui_homewidget.h"
+
+HomeWidget::HomeWidget(QWidget *parent) :
+    QWidget(parent), ui(new Ui::HomeWidget) {
+
+    ui->setupUi(this);
+
+    // Close window when quitButton is clicked
+    connect(ui->quitButton, SIGNAL(clicked()), this, SLOT(close()));
+
+    // Navigate to embedwindow when embedButton is clicked
+    connect(ui->embedButton, &QPushButton::clicked, this, [=]() {
+        // Show embedPage
+        ui->stackedWidget->setCurrentIndex(0);
+    });
+
+    connect(ui->comboBox, &QComboBox::activated,
+            ui->stackedWidget, &QStackedWidget::setCurrentIndex);
+}
+
+HomeWidget::~HomeWidget() {
+    delete ui;
+}
