3ef0966060ce47a3563d7aa366306c351e9adaf5
Joseph Diza <josephm.diza@gmail.com>
2022-08-11 07:44:08 -0700

    Port cli to argp over manually handling argv
    Argp is well-tested and flexible enough to be as useful and concise as
    manually handling the arguments through argv. Note that as a result of
    this move, the oggex app is broken and segfaults when invoked.

---
+++ app/cli.c
@@ -0,0 +1,68 @@
+#include "cli.h"
+#include "Version.h"
+
+struct arguments set_default_args() {
+  struct arguments arguments;
+
+  /* Default values. */
+  arguments.verbose = 0;
+  arguments.scale = 1000;
+  arguments.readable = false;
+  return arguments;
+}
+
+error_t parse_opt (int key, char *arg, struct argp_state *state) {
+  /* Get the input argument from argp_parse, which we
+     know is a pointer to our arguments structure. */
+  struct arguments *arguments = state->input;
+  switch (key) {
+    case 'V':
+      printf("oggex %s v%d.%d.%d\n", email, VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
+      exit(0);
+
+    case 'v':
+      arguments->verbose = 1;
+      break;
+
+    case 'a':
+      arguments->sound.src = arg;
+      break;
+
+    case 'i':
+      arguments->sound.dest = arg;
+      break;
+
+    case 't':
+      arguments->sound.tag = arg;
+      break;
+
+    case 'r':
+      arguments->readable = true;
+      break;
+    
+    case 500:
+      arguments->scale = 1000;
+
+    case 600:
+      arguments->scale = 1024;
+
+    case ARGP_KEY_ARG:
+      if (state->arg_num > 1)
+        /* Too many arguments. */
+        argp_usage (state);
+      arguments->args[state->arg_num] = arg;
+      break;
+
+    case ARGP_KEY_END:
+      if (state->arg_num < 1) {
+        /* Not enough arguments. */
+        argp_usage (state);
+      }
+      break;
+
+    default:
+      return ARGP_ERR_UNKNOWN;
+
+    }
+  return 0;
+}
---
+++ include/cli.h
@@ -0,0 +1,69 @@
+#pragma once
+#ifndef OGGEX_CLI_H
+#define OGGEX_CLI_H
+
+//#include "media.h"
+#include "sound.h"
+//#include "Version.h"
+
+/* Support header include for C++ */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <argp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <pcre.h>
+#include <math.h>
+
+/** Author and maintainer's email. Contact me directly here for questions or bug reports, or use the issues page. */
+static const char *email = "<josephm.diza@gmail.com>";
+
+/** Program summary. */
+static const char doc[] = "Oggex - Embed and extract OGG audio files in images using sound tags";
+
+/** A description of the arguments we accept. */
+static const char args_doc[] = "[embed/extract] [options]";
+
+/** http://www.gnu.org/software/libc/manual/html_node/Argp-Option-Vectors.html <br> */
+/** The options we understand. */
+static const struct argp_option options[] = {
+  {"version"  , 'V', 0, 0, "Show program version", 0},
+  {"verbose"  , 'v', 0, 0, "Display program output and debugging information", 0},
+  {"audio"    , 'a', 0, 0, "Sound file path", 0},
+  {"image"    , 'i', 0, 0, "Image file path", 0},
+  {"tag"      , 't', 0, 0, "Sound tag caption", 0},
+  {"readable" , 'r', 0, 0, "Display human readable file sizes in logs", 0},
+  //{"format"   , 400, 0, 0, "Use either SI or binary unit sizes in logs", 0},
+  {"si"     ,  500, 0,      0, "Display SI byte sizes", 0},
+  {"bi"     ,  600, 0,      0, "Display binary byte sizes", 0},
+};
+
+/** Used by main to communicate with parse_opt. */
+struct arguments {
+  char *args[1]; /** from, to */
+  bool display_units;
+  int verbose;
+  int scale;
+  bool readable;
+  struct Sound sound;
+};
+
+/** Parse a single option. */
+error_t parse_opt (int key, char *arg, struct argp_state *state);
+
+/** http://www.gnu.org/software/libc/manual/html_node/Argp-Parsers.html <br> */
+/** http://www.gnu.org/software/libc/manual/html_node/Argp-Help-Filtering.html <br> */
+/** Our argp parser. */
+static const struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0};
+
+/** Set default values for arguments */
+struct arguments set_default_args();
+
+#ifdef __cplusplus
+}
+#endif
+#endif // OGGEX_CLI_H
