511e6ed3af4d76ccf13128390bfafcf4f0268e63
Joseph Diza <josephm.diza@gmail.com>
2022-08-21 00:32:57 -0700

    Remove dead spdlog code

--- app/oggex-bin.cpp
+++ app/oggex-bin.cpp
@@ -3,27 +3,24 @@
 #include <spdlog/logger.h>
 #include <spdlog/cfg/env.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
-#include <spdlog/sinks/daily_file_sink.h>
 #include <spdlog/sinks/stdout_sinks.h>
 
 int main(int argc, char **argv) {
-
   // Parse arguments
   struct arguments arguments = set_default_args();
   argp_parse (&argp, argc, argv, 0, 0, &arguments);
 
   char* command = arguments.args[0];
-
   Sound sound = arguments.sound;
   Settings settings = { 10, false };
   Media media = {sound, settings, arguments};
 
-  // Setup libary logging sinks
+  // Setup library logging
   std::vector<spdlog::sink_ptr> sinks;
   sinks.push_back(std::make_shared<spdlog::sinks::stdout_sink_st>());
-
   auto logger = library::setup_logger(sinks);
 
+  // Enable/disable logging
   if (arguments.verbose == 1)
     spdlog::set_level(spdlog::level::trace);
   else
@@ -33,32 +30,22 @@ int main(int argc, char **argv) {
     SPDLOG_ERROR("You must provide a valid image file. Supported image formats are: PNG, JPG, JPEG and GIF.");
     exit(-1);
   }
-    //library::error("You must provide a valid image file. Supported image formats are: PNG, JPG, JPEG and GIF.");
-    //l_app->error("You must provide a valid image file. Supported image formats are: PNG, JPG, JPEG and GIF.");
   
-  // Handle commands
+  // Handle oggex commands
   if (strcmp(command, "embed") == 0) {
-    //if (sound.src != nullptr && !file_exists(sound.src))
-    if (sound.src == NULL || !file_exists(sound.src)) {
+    if (sound.src == nullptr || !file_exists(sound.src)) {
       SPDLOG_ERROR("You must provide a valid .ogg audio file.");
       exit(-1);
     }
-      //library::error("You must provide a valid .ogg audio file.");
-      //l_app->error("You must provide a valid .ogg audio file.");
 
     if (sizeof(sound.tag) == 0) {
       SPDLOG_ERROR("You cannot have an empty sound tag");
       exit(-1);
     }
-      //library::error("You cannot have an empty sound tag");
-      //l_app->error("You cannot have an empty sound tag");
 
-    //spdlog::shutdown();
     embed(media);
-  } else if (strcmp(command, "extract") == 0) {
-    //spdlog::shutdown();
+  } else if (strcmp(command, "extract") == 0)
     extract(media);
-  }
 
   return 0;
 }
--- src/ogxlog.cpp
+++ src/ogxlog.cpp
@@ -4,10 +4,9 @@
 #include <spdlog/sinks/stdout_color_sinks.h>
 
 namespace library {
-  //static const std::string logger_name = log_lib;
   static const std::string logger_name = "oggex";
 
-  // Registers the libary logger for 
+  // Registers the libary logger
   std::shared_ptr<spdlog::logger> setup_logger(std::vector<spdlog::sink_ptr> sinks) {
       auto logger = spdlog::get(logger_name);
       if (!logger) {
@@ -20,39 +19,4 @@ namespace library {
       }
       return logger;
   }
-
-  //void trace(std::string message) {
-      //auto logger = spdlog::get(logger_name);
-      //if (logger) {
-          //logger->trace("{}::{}", __FUNCTION__, message);
-      //}
-  //}
-
-  //void info(std::string message) {
-      //auto logger = spdlog::get(logger_name);
-      //if (logger) {
-          //logger->info("{}::{}", __FUNCTION__, message);
-      //}
-  //}
-
-  //void warn(std::string message) {
-      //auto logger = spdlog::get(logger_name);
-      //if (logger) {
-          //logger->warn("{}::{}", __FUNCTION__, message);
-      //}
-  //}
-
-  //void debug(std::string message) {
-      //auto logger = spdlog::get(logger_name);
-      //if (logger) {
-          //logger->debug("{}::{}", __FUNCTION__, message);
-      //}
-  //}
-
-  //void error(std::string message) {
-      //auto logger = spdlog::get(logger_name);
-      //if (logger) {
-          //logger->warn("{}::{}", __FUNCTION__, message);
-      //}
-  //}
 }
