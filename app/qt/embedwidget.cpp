781ebeafd3f7bfc5233df01f885630e1080870c0
Joseph Diza <josephm.diza@gmail.com>
2022-08-24 23:44:20 -0700

    Move logging setup to oggex-qt mainwindow application point

--- app/qt/embedwidget.cpp
+++ app/qt/embedwidget.cpp
@@ -1,27 +1,7 @@
 #include "embedwidget.h"
 
-std::shared_ptr<spdlog::logger> setup_logger(std::vector<spdlog::sink_ptr> sinks) {
-  auto logger = spdlog::get(logger_name);
-  if (!logger) {
-    if (sinks.size() > 0) {
-        logger = std::make_shared<spdlog::logger>(logger_name, std::begin(sinks), std::end(sinks));
-        spdlog::register_logger(logger);
-    } else {
-        logger = spdlog::stdout_color_mt(logger_name);
-    }
-  }
-  return logger;
-}
-
 EmbedWidget::EmbedWidget(QWidget *parent)
     : QWidget(parent), ui(new Ui::EmbedWidget) {
-
-    std::vector<spdlog::sink_ptr> sinks;
-    sinks.push_back(std::make_shared<spdlog::sinks::stdout_sink_st>());
-    auto logger = setup_logger(sinks);
-
-    // For now, enable logs
-    spdlog::set_level(spdlog::level::level_enum::trace);
 
     ui->setupUi(this);
 
--- app/qt/oggex-qt.cpp
+++ app/qt/oggex-qt.cpp
@@ -1,6 +1,27 @@
 #include "mainwindow.h"
 
+std::shared_ptr<spdlog::logger> setup_logger(std::vector<spdlog::sink_ptr> sinks) {
+  auto logger = spdlog::get(logger_name);
+  if (!logger) {
+    if (sinks.size() > 0) {
+        logger = std::make_shared<spdlog::logger>(logger_name, std::begin(sinks), std::end(sinks));
+        spdlog::register_logger(logger);
+    } else {
+        logger = spdlog::stdout_color_mt(logger_name);
+    }
+  }
+  return logger;
+}
+
 int main(int argc, char *argv[]) {
+    // Setup logging
+    std::vector<spdlog::sink_ptr> sinks;
+    sinks.push_back(std::make_shared<spdlog::sinks::stdout_sink_st>());
+    auto logger = setup_logger(sinks);
+
+    // For now, enable logs
+    spdlog::set_level(spdlog::level::level_enum::trace);
+
     QApplication app(argc, argv);
     MainWindow mainWindow;
 
