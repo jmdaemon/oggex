23a1530b376e7b4dd05e123ff9dedc65667b57a0
Joseph Diza <josephm.diza@gmail.com>
2022-08-25 02:43:03 -0700

    Fix quit button

--- app/qt/homewidget.cpp
+++ app/qt/homewidget.cpp
@@ -1,4 +1,5 @@
 #include "homewidget.h"
+#include <QApplication>
 
 HomeWidget::HomeWidget(QWidget *parent) :
     QWidget(parent), ui(new Ui::HomeWidget) {
@@ -6,7 +7,9 @@ HomeWidget::HomeWidget(QWidget *parent) :
     ui->setupUi(this);
 
     // Close window when quitButton is clicked
-    connect(ui->quitButton, SIGNAL(clicked()), this, SLOT(close()));
+    //connect(ui->quitButton, SIGNAL(clicked()), this, SLOT(close()));
+    //connect(ui->quitButton, SIGNAL(clicked()), this, SLOT(QApplication::quit()));
+    connect(ui->quitButton, &QPushButton::clicked, this, &QApplication::quit);
 
     // Navigate to embedwindow when embedButton is clicked
     connect(ui->embedButton, &QPushButton::clicked, this, [=, this]() {
@@ -16,6 +19,10 @@ HomeWidget::HomeWidget(QWidget *parent) :
 
     connect(ui->comboBox, &QComboBox::activated,
             ui->stackedWidget, &QStackedWidget::setCurrentIndex);
+}
+
+QPushButton* HomeWidget::getQuitButton() {
+    return this->ui->embedButton;
 }
 
 HomeWidget::~HomeWidget() {
