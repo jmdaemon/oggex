8bf3dc595ad1add8c64d365e72c2be3168462c4d
Joseph Diza <josephm.diza@gmail.com>
2022-08-24 18:33:52 -0700

    Fix bug where audio sound tag was not correctly stripped

--- src/oggex.cpp
+++ src/oggex.cpp
@@ -112,7 +112,7 @@ std::string find_sound_tag(std::string conts) {
   }
   auto tag = conts.substr(lb, rb);                  // [audio02] 
   trim(tag);
-  auto stripped = tag.substr(1, tag.length() - 2);  // [audio02] => audio02
+  auto stripped = tag.substr(1, tag.length() - 3);  // [audio02] => audio02
 
   SPDLOG_DEBUG("Stripped  : {}", tag);
   SPDLOG_DEBUG("Unstripped: {}", stripped);
