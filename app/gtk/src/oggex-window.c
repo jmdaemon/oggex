5f7f5b7bf0ffde9bc150c2f8299679200e3c6cb0
Joseph Diza <josephm.diza@gmail.com>
2022-08-28 14:47:43 -0700

    Refactor UI template variable names into global constants
    Even though they're dynamically allocated, doing it this way is more
    resilient to structural changes.

--- app/gtk/src/oggex-window.c
+++ app/gtk/src/oggex-window.c
@@ -1,5 +1,35 @@
 #include "oggex-config.h"
 #include "oggex-window.h"
+#include "oggex_gtk_defs.h"
+
+const char* get_ui_resource(const char* ui_file) {
+  // Calculate the final path size
+
+  unsigned int fname_size = strlen(APP_ID) + strlen(RESOURCES_DIR) + strlen(RESOURCES_GTK_DIR) + 3;
+
+  char* result = malloc(fname_size);
+  /*char result [fname_size + 1];*/
+  /*result = "";*/
+  /*char* result;*/
+  /*result = strcat(result, APP_ID);*/
+  /*result = strcat(result, "/");*/
+  /*result = strcat(result, RESOURCES_DIR);*/
+  /*result = strcat(result, "/");*/
+  /*result = strcat(result, RESOURCES_GTK_DIR);*/
+  /*result = strcat(result, "/");*/
+  /*result = strcat(result, ui_file);*/
+
+  /*strcat(result, APP_ID);*/
+  /*strcat(result, "/");*/
+  /*strcat(result, RESOURCES_DIR);*/
+  /*strcat(result, "/");*/
+  /*strcat(result, RESOURCES_GTK_DIR);*/
+  /*strcat(result, "/");*/
+  /*strcat(result, ui_file);*/
+
+  sprintf(result, "%s/%s/%s/%s", APP_ID, RESOURCES_DIR, RESOURCES_GTK_DIR, ui_file);
+  return result;
+}
 
 struct _OggexWindow {
   GtkApplicationWindow  parent_instance;
@@ -14,9 +44,12 @@ G_DEFINE_TYPE (OggexWindow, oggex_window, GTK_TYPE_APPLICATION_WINDOW)
 static void oggex_window_class_init (OggexWindowClass *klass) {
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
 
-  gtk_widget_class_set_template_from_resource (widget_class, "/io/github/com/jmdaemon/oggex/../../../resources/gtk/oggex-window.ui");
+  /*gtk_widget_class_set_template_from_resource (widget_class, "/io/github/com/jmdaemon/oggex/../../../resources/gtk/oggex-window.ui");*/
+  const char* resource = get_ui_resource(UI_OGGEX_WINDOW);
+  gtk_widget_class_set_template_from_resource (widget_class, resource);
   gtk_widget_class_bind_template_child (widget_class, OggexWindow, header_bar);
   gtk_widget_class_bind_template_child (widget_class, OggexWindow, label);
+  /*free(resource);*/
 }
 
 static void oggex_window_init (OggexWindow *self) {
---
+++ include/gtk/oggex_gtk_defs.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef OGGEX_GTK_DEFS_H
+#define OGGEX_GTK_DEFS_H
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static const char* APP_ID = "/io/github/com/jmdaemon/oggex";
+static const char* RESOURCES_DIR = "../../../resources";
+static const char* RESOURCES_GTK_DIR = "gtk";
+
+/* UI Files */
+static const char* UI_OGGEX_WINDOW = "oggex-window.ui";
+static const char* UI_HELP_OVERLAY = "help-overlay.ui";
+
+#endif // OGGEX_GTK_DEFS_H
