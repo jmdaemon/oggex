fe1aeed6027eb03663ce96ce00abf402ec4b3f70
Joseph Diza <josephm.diza@gmail.com>
2022-08-28 11:29:31 -0700

    Add initial gtk app version

---
+++ app/gtk/src/oggex-window.c
@@ -0,0 +1,24 @@
+#include "oggex-config.h"
+#include "oggex-window.h"
+
+struct _OggexWindow {
+  GtkApplicationWindow  parent_instance;
+
+  /* Template widgets */
+  GtkHeaderBar        *header_bar;
+  GtkLabel            *label;
+};
+
+G_DEFINE_TYPE (OggexWindow, oggex_window, GTK_TYPE_APPLICATION_WINDOW)
+
+static void oggex_window_class_init (OggexWindowClass *klass) {
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  gtk_widget_class_set_template_from_resource (widget_class, "/io/github/com/jmdaemon/oggex/../../../resources/gtk/oggex-window.ui");
+  gtk_widget_class_bind_template_child (widget_class, OggexWindow, header_bar);
+  gtk_widget_class_bind_template_child (widget_class, OggexWindow, label);
+}
+
+static void oggex_window_init (OggexWindow *self) {
+  gtk_widget_init_template (GTK_WIDGET (self));
+}
