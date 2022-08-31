1724ef1b80c70459cddcf76cf132a6dc5b549c4a
Joseph Diza <josephm.diza@gmail.com>
2022-08-31 03:24:46 -0700

    Use FileChooserButton widget in place of GtkButton
    This is the bare minimum required to use and display the widget without
    the program segfaulting. Note that none of the signal handlers are
    connected yet so this is also not that useful at the moment.

--- app/gtk/src/embedwidget.c
+++ app/gtk/src/embedwidget.c
@@ -13,6 +13,14 @@ static void embed_widget_class_init (EmbedWidgetClass *klass) {
   const char* resource = "/io/github/com/jmdaemon/oggex/../../../resources/gtk/embedwidget.ui";
   gtk_widget_class_set_template_from_resource (widget_class, resource);
 
+  gtk_widget_class_bind_template_child(widget_class, EmbedWidgetClass, lbl_dest);
+  gtk_widget_class_bind_template_child(widget_class, EmbedWidgetClass, lbl_tag);
+  gtk_widget_class_bind_template_child(widget_class, EmbedWidgetClass, lbl_image);
+  gtk_widget_class_bind_template_child(widget_class, EmbedWidgetClass, lbl_audio);
+  gtk_widget_class_bind_template_child(widget_class, EmbedWidgetClass, lbl_embed);
+  gtk_widget_class_bind_template_child(widget_class, EmbedWidgetClass, e_tag);
+  gtk_widget_class_bind_template_child(widget_class, EmbedWidgetClass, e_dest);
+  gtk_widget_class_bind_template_child(widget_class, EmbedWidgetClass, btn_embed);
   /*free(resource);*/
 }
 
@@ -20,6 +28,167 @@ static void embed_widget_init (EmbedWidget *self) {
   gtk_widget_init_template (GTK_WIDGET (self));
 }
 
+/*#define EMBED_WIDGET(obj) (G_TYPE_CHECK_INSTANCE_CAST((obj), EMBED_WIDGET_TYPE_WIDGET, EmbedWidget))*/
+
 EmbedWidget* embed_widget_new (void) {
-  return g_object_new (EMBED_WIDGET_TYPE_WIDGET, NULL);
+  EmbedWidget* result = g_object_new (EMBED_WIDGET_TYPE_WIDGET, NULL);
+  
+  /*embed_widget_init(result);*/
+  /*embed_widget_class_init(OGGEX_EMBED_WIDGET_CLASS(result));*/
+
+  /*EmbedWidgetClass* widget_class = OGGEX_EMBED_WIDGET_CLASS(result);*/
+
+  /*embed_widget_init(result);*/
+  /*EmbedWidgetClass* widget_class = OGGEX_EMBED_WIDGET_GET_CLASS(result);*/
+  /*EmbedWidgetClass* asdf = embed_widget_get_type();*/
+
+
+  /*EmbedWidgetClass* widget_class = OGGEX_EMBED_WIDGET_CLASS(result);*/
+  /*G_TYPE_INSTANCE_GET_CLASS(result, EMBED_WIDGET_TYPE_WIDGET, EmbedWidgetClass);*/
+
+  /*EmbedWidgetClass* widget_class = EmbedWidgetClass(result);*/
+  /*EmbedWidgetClass* widget_class = EMBED_WIDGET(result);*/
+
+  /*widget_class->parent_instance*/
+
+  puts("Before");
+  /*EmbedWidgetClass* widget_class = OGGEX_EMBED_WIDGET_CLASS(result);*/
+  /*GtkWidgetClass* widget_class = (EmbedWidgetClass*) OGGEX_EMBED_WIDGET_CLASS(result);*/
+  /*EmbedWidgetClass* widget_class = OGGEX_EMBED_WIDGET_CLASS(self);*/
+
+  /*GtkWidgetClass* widget_class = (EmbedWidgetClass*) OGGEX_EMBED_WIDGET_CLASS(result);*/
+  /*GtkWidgetClass* widget_class = G_TYPE_INSTANCE_GET_CLASS(result, EMBED_WIDGET_TYPE_WIDGET, EmbedWidgetClass);*/
+
+  /*EmbedWidgetClass* widget_class = G_TYPE_INSTANCE_GET_CLASS(result, EMBED_WIDGET_TYPE_WIDGET, EmbedWidgetClass);*/
+
+  EmbedWidgetClass* widget_class = OGGEX_EMBED_WIDGET_GET_CLASS(result);
+  /*GtkWidgetClass *klass = GTK_WIDGET_CLASS (widget_class);*/
+  /*OGGEX_EMBED_WIDGET_CLASS(result)*/
+  /*EmbedWidgetClass* widget_class = OGGEX_EMBED_WIDGET_CLASS(result);*/
+
+  puts("After");
+  
+  /*if (OGGEX_IS_EMBED_WIDGET_CLASS(widget_class) != 0) {*/
+    /*fprintf(stderr, "Embed Widget class is null. Exiting...");*/
+    /*exit(1);*/
+  /*}*/
+
+  puts("Initializing fcb_image");
+  widget_class->fcb_image = filechooserbutton_new("Open Image", "Image Files (*.png *.jpg *.bmp)", GTK_FILE_CHOOSER_ACTION_OPEN);
+  printf("fcb_image %p\n", widget_class->fcb_image);
+  /*widget_class->fcb_audio->parent_instance = *GTK_BUTTON(widget_class->fcb_image);*/
+
+  puts("Initializing fcb_audio");
+  widget_class->fcb_audio = filechooserbutton_new("Open Audio", "Audio Files (*.ogg)", GTK_FILE_CHOOSER_ACTION_OPEN);
+  printf("fcb_audio %p\n", widget_class->fcb_audio);
+  /*widget_class->fcb_audio->parent_instance = *GTK_BUTTON(widget_class->fcb_audio);*/
+
+  puts("Adding fcb_image to Grid");
+  /*GtkWidget* widget = GTK_WIDGET(widget_class->fcb_image);*/
+  /*gtk_grid_attach(&result->parent_instance, widget, 1, 1, 1, 1);*/
+  /*gtk_grid_attach(&result->parent_instance, GTK_WIDGET(GTK_GRID(widget_class->fcb_image)), 1, 1, 1, 1);*/
+  /*GtkWidget* widget = GTK_WIDGET(&widget_class->fcb_image->parent_instance);*/
+  /*GObject* widget = G_OBJECT(widget_class->fcb_image);*/
+  /*gtk_grid_attach(&result->parent_instance, widget, 1, 1, 1, 1);*/
+  /*GtkWidget* widget = GTK_WIDGET(widget_class->fcb_image->parent_instance->parent_instance);*/
+
+  GtkGrid* grid = NULL;
+  /*grid = &result->parent_instance;*/
+  grid = &result->parent_instance;
+  if (!grid) {
+    fprintf(stderr, "embed_grid is null. Exiting...");
+    exit(2);
+  }
+  printf("grid %p\n", grid);
+
+  /*GtkButton btn = NULL;*/
+  GtkButton* btn = NULL;
+  /*GtkButton btn;*/
+  btn = &widget_class->fcb_image->parent_instance;
+  /*btn = &widget_class->fcb_image->parent_instance;*/
+  /*btn = &widget_class->fcb_image->parent_instance;*/
+  /*btn = GTK_BUTTON(&widget_class->fcb_image);*/
+  /*btn = GTK_BUTTON(gtk_widget_get_root(GTK_WIDGET(widget_class->fcb_image)));*/
+  /*btn = GTK_BUTTON(gtk_widget_get_root(GTK_WIDGET(widget_class->fcb_image)));*/
+
+  /*GtkRoot* root = gtk_widget_get_root(GTK_WIDGET(widget_class->fcb_image));*/
+  /*btn = GTK_BUTTON(root);*/
+
+  if (!btn) {
+    fprintf(stderr, "btn is null. Exiting...");
+    exit(3);
+  }
+  printf("btn %p\n", btn);
+
+  GtkWidget* widget = NULL;
+  /*widget = GTK_WIDGET(btn);*/
+  /*widget = GTK_WIDGET(btn);*/
+  /*widget = GTK_WIDGET(&widget_class->fcb_image->parent_instance);*/
+  /*widget = GTK_WIDGET(&widget_class->fcb_image);*/
+  /*widget = &widget_class->fcb_image->parent_instance.parent_instance;*/
+  /*widget = GTK_WIDGET(&widget_class->fcb_image->parent_instance.parent_instance);*/
+  widget = &widget_class->fcb_image->parent_instance.parent_instance;
+  if (!widget) {
+    fprintf(stderr, "widget is null. Exiting...");
+    exit(4);
+  }
+
+  GtkButton button;
+
+  /*
+  if(GTK_IS_WIDGET_CLASS(btn)) {
+    widget = GTK_WIDGET(btn);
+    if (!widget) {
+      fprintf(stderr, "widget is null. Exiting...");
+      exit(4);
+    }
+  }
+  */
+  printf("widget %p\n", widget);
+    /*&widget_class->fcb_image->parent_instance.parent_instance;*/
+  /*GtkWidget *widget = &widget_class->fcb_image->parent_instance.parent_instance;*/
+  /*gtk_grid_attach(&result->parent_instance, widget, 1, 1, 1, 1);*/
+  /*gtk_grid_attach(grid, widget, 1, 1, 1, 1);*/
+  /**gtk_grid_attach(grid, widget, 1, 1, 1, 1);*/
+  /*gtk_grid_attach(&result->parent_instance, GTK_WIDGET(&widget_class->fcb_image->parent_instance), 1, 1, 1, 1);*/
+  /*gtk_grid_attach(&result->parent_instance, widget, 1, 1, 1, 1);*/
+
+  /*gtk_grid_attach(grid, widget, 1, 1, 1, 1);*/
+  /*gtk_grid_attach(grid, GTK_WIDGET(&button), 1, 1, 1, 1);*/
+  gtk_grid_attach(grid, GTK_WIDGET(btn), 1, 1, 1, 1);
+
+  puts("Adding fcb_audio to Grid");
+  /*gtk_grid_attach(&result->parent_instance, GTK_WIDGET(widget_class->fcb_audio), 1, 2, 1, 1);*/
+
+  /*EmbedWidgetClass* widget_class = G_TYPE_INSTANCE_GET_CLASS(result, EMBED_WIDGET_TYPE_WIDGET, EmbedWidgetClass);*/
+  
+  /*result->fcb_image;*/
+
+  /*GtkFileFilter *filter = gtk_file_filter_new ();*/
+  /*gtk_file_filter_add_pattern (filter, "*");*/
+
+  /*GtkFileFilter();*/
+  /*gtk_file_chooser_add_filter();*/
+
+  /*gtk_file_filter_get_name*/
+
+  /*GtkFileChooser *chooser = GTK_FILE_CHOOSER(filechooserbutton_get_fcn(widget_class->fcb_image));*/
+
+
+
+  /*EmbedWidgetPrivate* priv = G_TYPE_CLASS_GET_PRIVATE(result, EMBED_WIDGET_TYPE_WIDGET, EmbedWidgetPrivate);*/
+  /*EmbedWidgetClass* priv = EMBED_WIDGET_TYPE_WIDGET(result, EMBED_WIDGET_TYPE_WIDGET);*/
+  /*priv->btn = filechooserbutton_new("");*/
+  /*return g_object_new (EMBED_WIDGET_TYPE_WIDGET, NULL);*/
+
+  /* Add the file chooser widgets to the embed_widget user interface */
+  /*gtk_grid_attach(widget_class->parent_instance, widget_class->fcb_image, 1, 1, 1, 1);*/
+  /*gtk_grid_attach(&result->parent_instance, widget_class->fcb_image, 1, 1, 1, 1);*/
+
+
+  /*gtk_grid_attach(&self->parent_instance, GTK_WIDGET(widget_class->fcb_image), 1, 1, 1, 1);*/
+  /*gtk_grid_attach(&self->parent_instance, GTK_WIDGET(widget_class->fcb_audio), 1, 2, 1, 1);*/
+
+
+  return result;
 }
--- include/gtk/embedwidget.h
+++ include/gtk/embedwidget.h
@@ -2,6 +2,8 @@
 #ifndef OGGEX_EMBEDWIDGET_H
 #define OGGEX_EMBEDWIDGET_H
 
+#include "filechooserbutton.h"
+
 #include <gtk/gtk.h>
 
 G_BEGIN_DECLS
@@ -15,12 +17,18 @@ struct _EmbedWidgetClass {
   /* Template widgets */
   GtkLabel              *lbl_dest;
   GtkLabel              *lbl_tag;
-  GtkLabel              *lbl_audio;
   GtkLabel              *lbl_image;
+  FileChooserButton     *fcb_image;
+  GtkLabel              *lbl_audio;
+  FileChooserButton     *fcb_audio;
   GtkLabel              *lbl_embed;
   GtkEntry              *e_tag;
   GtkEntry              *e_dest;
   GtkButton             *btn_embed;
+
+  /* Custom Widgets */
+  //FileChooserButton* fcb_image;
+  //FileChooserButton* fcb_audio;
 };
 
 EmbedWidget* embed_widget_new (void);
