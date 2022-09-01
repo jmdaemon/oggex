411ad7587babf23e5f3561732280ebbe78c47bdb
Joseph Diza <josephm.diza@gmail.com>
2022-08-31 19:27:05 -0700

    Single source APP_ID

--- app/gtk/src/main.cpp
+++ app/gtk/src/main.cpp
@@ -3,39 +3,12 @@
 
 #include "oggex-config.h"
 #include "oggex-application.h"
-
-//#include "cli.h"
-//#include "ogx.h"
 #include "oggex_gtk.h"
 
-/* Dummy cli parser to appease GLib */
-static int command_line (GApplication *application, GApplicationCommandLine *cmdline) {
-	//gchar **argv;
-	//gint argc;
-	//gint i;
-
-	//g_application_hold(application);
+/* Disable command line parsing with glib */
+static int command_line (GApplication *application, GApplicationCommandLine /* *cmdline */) {
+	// Show gtk app
 	g_application_activate(application);
-
-	//args.args[0] = (char*) "";
-	//args = set_default_args();
-	//args.verbose = 1;
-	//setup_logging(args);
-
-	//g_object_unref (cmdline);
-
-	//argv = g_application_command_line_get_arguments (cmdline, &argc);
-	//argv = g_application_command_line_get_arguments (argp, &argc);
-
-  //g_application_command_line_print (cmdline,
-                                    //"This text is written back\n"
-                                    //"to stdout of the caller\n");
-
-	//for (i = 0; i < argc; i++)
-		//g_print ("argument %d: %s\n", i, argv[i]);
-
-	//g_strfreev (argv);
-
   return 0;
 }
 
@@ -48,10 +21,7 @@ int main (int argc, char **argv) {
 	args.nolimit = true;
 	args.verbose = 1;
 	setup_logging(args);
-
-	//args = set_default_args();
-	//args.verbose = 1;
-	//setup_logging(args);
+	// TODO: Parse command line args here
 
 	// Ideal arg parsing setup code
 	//args.args[0] = (char*) "";
@@ -62,18 +32,6 @@ int main (int argc, char **argv) {
 	//char* command = args.args[0];
 	//Media media = {args.sound, args.settings, args};
 	//setup_logging(args);
-
-	//oggex(command, media);
-
-  //Media media = {arguments.sound, arguments.settings, arguments};
-  //setup_logging(arguments);
-  //oggex(command, media);
-
-
-  //char* command = arguments.args[0];
-  //Media media = {arguments.sound, arguments.settings, arguments};
-  //setup_logging(arguments);
-  //oggex(command, media);
 
 	g_autoptr(OggexApplication) app = NULL;
 	int ret;
@@ -88,9 +46,7 @@ int main (int argc, char **argv) {
 	 * application windows, integration with the window manager/compositor, and
 	 * desktop features such as file opening and single-instance applications.
 	 */
-	//app = oggex_application_new ("io.github.com.jmdaemon.oggex", G_APPLICATION_FLAGS_NONE);
-	//app = oggex_application_new ("io.github.com.jmdaemon.oggex", G_APPLICATION_HANDLES_COMMAND_LINE);
-	app = oggex_application_new ((gchar*) "io.github.com.jmdaemon.oggex", G_APPLICATION_HANDLES_COMMAND_LINE);
+	app = oggex_application_new ((gchar*) APP_ID, G_APPLICATION_HANDLES_COMMAND_LINE);
 	g_signal_connect (app, "command-line", G_CALLBACK (command_line), NULL);
 
 	/*
@@ -103,11 +59,6 @@ int main (int argc, char **argv) {
 	 * method "run". But we need to cast, which is what the "G_APPLICATION()"
 	 * macro does.
 	 */
-	//ret = g_application_run (G_APPLICATION (app), 0, NULL);
-	//ret = g_application_run (G_APPLICATION (app), argc, argv);
-	//ret = g_application_run (G_APPLICATION (app), 1, argv);
-	//ret = g_application_run (G_APPLICATION (app), argc, argv);
-	//ret = g_application_run (G_APPLICATION (app), 1, argv);
 	ret = g_application_run (G_APPLICATION (app), NULL, NULL);
 
 	return ret;
