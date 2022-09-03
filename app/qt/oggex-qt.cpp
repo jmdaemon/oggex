3e1a2577ec61df0839413b021cdb1086815dc619
Joseph Diza <josephm.diza@gmail.com>
2022-09-02 18:21:27 -0700

    Readd pure cli functionality into oggex-qt
    Before, oggex-qt would ignore commands from the command line and
    would only show the user interface. Now you can use oggex-qt the same as
    you would oggex and they will work the exact same.

--- app/qt/oggex-qt.cpp
+++ app/qt/oggex-qt.cpp
@@ -2,10 +2,15 @@
 #include "oggex_qt.h"
 
 int main(int argc, char **argv) {
-    args.args[0] = (char*) "";
+    args.args[0] = (char*) "placeholder";
     args = set_default_args();
-    args.verbose = 1;
+    argp_parse(&argp, argc, argv, 0, 0, &args);
     setup_logging(args);
+
+    Media media = {args.sound, args.settings, args};
+    if (strcmp(args.args[0], "placeholder") != 0) {
+        return oggex(args.args[0], media);
+    }
 
     QApplication app(argc, argv);
     MainWindow mainWindow;
