ddd0bfe2f3098f5063340c69b34389898f6dc206
Joseph Diza <josephm.diza@gmail.com>
2022-09-02 19:00:13 -0700

    Reduce code across gui binaries with init_args_cli

--- app/oggex-bin.cpp
+++ app/oggex-bin.cpp
@@ -1,5 +1,6 @@
 #include "ogx.h"
 
+/** Initializes the spdlog logger */
 std::shared_ptr<spdlog::logger> setup_logger(std::vector<spdlog::sink_ptr> sinks) {
   auto logger = spdlog::get(logger_name);
   if (!logger) {
@@ -13,6 +14,7 @@ std::shared_ptr<spdlog::logger> setup_logger(std::vector<spdlog::sink_ptr> sinks
   return logger;
 }
 
+/** Initializes spdlog logger, and toggles logging statements at runtime */
 void setup_logging(arguments arguments) {
   // Setup library logging
   std::vector<spdlog::sink_ptr> sinks;
@@ -57,9 +59,40 @@ int oggex(const char* command, Media media) {
   return 0;
 }
 
+/** Parses command line arguments */
 arguments init_args(int argc, char** argv) {
-  // Parse arguments
   struct arguments arguments = set_default_args();
   argp_parse (&argp, argc, argv, 0, 0, &arguments);
   return arguments;
+}
+
+// For GUI executables
+const char* COMMAND_PLACEHOLDER = "placeholder";
+
+/** Initializes the arguments struct for gui binaries
+  * 
+  * Argp will error out if args.args[0] is null.
+  * This is intended functionality for oggex but not for oggex-qt or oggex-gtk.
+  * This function allows us to skip this check for gui binaries by providing a dummy placeholder
+  * value for the necessary arguments.
+  *
+  * At runtime in oggex-gtk or oggex-qt, we check for this placeholder to determine if we should
+  * do the embed/extract the same as oggex cli would, or to display the gui.
+  *
+  * Note that we cannot setup the logger here, since it will be linked later in
+  * the gui executables and will cause problems otherwise.
+  */
+arguments init_args_gui(int argc, char** argv) {
+  struct arguments args;
+  // Setup placeholder value
+	args.args[0] = (char*) COMMAND_PLACEHOLDER;
+	args = set_default_args();
+	argp_parse(&argp, argc, argv, 0, 0, &args);
+  return args;
+}
+
+/* Determine if the command was the placeholder value, or real user input */
+bool cmd_specified(struct arguments args) {
+  bool cmd_was_given = (strcmp(args.args[0], COMMAND_PLACEHOLDER) != 0) ? true : false;
+  return cmd_was_given;
 }
--- app/qt/oggex-qt.cpp
+++ app/qt/oggex-qt.cpp
@@ -2,15 +2,12 @@
 #include "oggex_qt.h"
 
 int main(int argc, char **argv) {
-    args.args[0] = (char*) "placeholder";
-    args = set_default_args();
-    argp_parse(&argp, argc, argv, 0, 0, &args);
+    //args = init_args(argc, argv);
+    args = init_args_gui(argc, argv);
     setup_logging(args);
-
     Media media = {args.sound, args.settings, args};
-    if (strcmp(args.args[0], "placeholder") != 0) {
-        return oggex(args.args[0], media);
-    }
+    if (cmd_specified(args))
+      return oggex(args.args[0], media);
 
     QApplication app(argc, argv);
     MainWindow mainWindow;
